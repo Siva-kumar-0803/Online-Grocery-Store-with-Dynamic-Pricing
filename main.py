db74a965022f0dee0a78f5d71f7140aa96d65451
Sivakumar <makemoney9047@gmail.com>
2024-09-07 22:40:32 -0700

    styling and image properties are fixed contact pageonly remaing remainin

--- main.py
+++ main.py
@@ -20,16 +20,47 @@ def blog():
 
 @app.route('/my account')
 def my_account():
-    return render_template('account.html')
+    return render_template('my account.html')
 
 @app.route('/whishlist')
 def whishlist():
     return render_template('whishlist.html')
 
-@app.route('/checkout')
-def checkout():
-    return render_template('checkout.html')
+@app.route('/add address')
+def add_address():
+    return render_template('add address.html')
 
+@app.route('/products')
+def product():
+    return render_template('products.html')
+
+@app.route('/single product')
+def single_product():
+    return render_template('single product.html')
+
+@app.route('/payment')
+def payment():
+    return render_template('payment.html')
+@app.route('/shopping cart')
+def shopping_cart():
+    return render_template('shopping cart.html')
+
+@app.route('/admin')
+def admin():
+    return render_template('admin.html')
+
+@app.route('/admin customer details')
+def admin_customer_details():
+    return render_template('admin customer details.html')
+
+@app.route('/admin order details')
+def admin_order_details():
+    return render_template('admin order details.html')
+
+
+@app.route('/admin add product')
+def admin_add_product():
+    return render_template('admin add product.html')
 
 
 if (__name__) == "__main__":
